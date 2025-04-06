b3fc9cf8af7b9b2fe7a4ff13c263ed584fe1ee61
cabdyn <ady.cbn@gmail.com>
2025-04-06 15:19:59 +0300

    set refresh rate to 60, created player as triangle, made player rotate left and right

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +18,9 @@ def main():
                 return
             
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
